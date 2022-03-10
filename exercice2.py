5e86f7255ad2b57a1ae41a1baa0c47cb1246c0c2
Allta <francois.delrue@ynov.com>
2022-03-10 10:53:15 +0100

    add source exo2

---
+++ exercice2.py
@@ -0,0 +1,30 @@
+
+import hashlib
+import requests 
+
+# Aller chercher le dictionnaire sur l'url  
+def readwordlist(url):
+ 
+
+#Fonction permettant de hasher le password pour le tester contre le password à trouver
+def hash(wordlistpassword):
+#    return result.hexdigest()
+ 
+ 
+def bruteforce(guesspasswordlist, actual_password_hash):
+    #Check si le password hashé est le même que password à trouver
+ 
+############# append the below code ################ 
+
+url = 'https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top12Thousand-probable-v2.txt'
+actual_password = 'volleyball'
+actual_password_hash = hash(actual_password)
+
+#Recuperation de la liste des passwords
+
+guesspasswordlist= 
+
+# Attaque Bruteforce
+bruteforce(guesspasswordlist, actual_password_hash)
+ 
+ 
